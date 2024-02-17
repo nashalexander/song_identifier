b45e0fe38e0057a91313fa136ca99fbbd8d2cf83
Alex Nash <contact@alexnash.dev>
2024-02-16 23:21:16 -0500

    Functioning exif script

--- rename_using_tags.sh
+++ rename_using_tags.sh
@@ -1,15 +1,41 @@
-file=${1:?}
-if exiftool ${file} | grep -q 'Title'; then
-  title=$(exiftool -Title -m -p '${Title}' ${file})
-else
-  echo "Title tag does not exist for ${file}."
+#!/bin/bash
+
+MV_CMD=mv
+
+# Dry run
+if [[ "$1" = "-d" ]]; then
+  MV_CMD=echo
+  shift
 fi
 
-if exiftool ${file} | grep -q 'Artist'; then
-  artist=$(exiftool -Title -m -p '${Artist}' ${file})
-else
-  echo "Title tag does not exist for ${file}."
-fi
+all_files="$@"
 
-echo "${title} - ${artist}"
+for file in ${all_files}; do
+  title=""
+  artist=""
 
+  if exiftool ${file} | grep -q 'Title'; then
+    title=$(exiftool -Title -m -p '${Title}' ${file})
+  else
+    continue
+  fi
+
+  if exiftool ${file} | grep -q 'Artist'; then
+    artist=$(exiftool -Title -m -p '${Artist}' ${file})
+  fi
+
+  file_ext="${file##*.}"
+
+  echo "${file} : ${title} - ${artist}.${file_ext}"
+  echo "rename file? (y/N)"
+  
+  read answer
+  if [[ "${answer}" = "y" ]] || [[ "${answer}" = "Y" ]]; then
+      file_ext="${file##*.}"
+      
+      $MV_CMD "${file}" "${title} - ${artist}.${file_ext}"
+  else
+      echo "${file} not renamed"
+  fi
+
+done
